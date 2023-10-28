b4424a9e7da286b863d278e08153dd5eb83ec50b
Stefan Claas <13186616+stefanclaas@users.noreply.github.com>
2023-10-28 19:58:20 +0200

    Add files via upload

--- sv.go
+++ sv.go
@@ -8,141 +8,179 @@ import (
 	"encoding/hex"
 	"os"
 	"bytes"
+	"strings"
+	"strconv"
 )
 
-// Signature Marker equals email or Usenet Signatures Marker,
-// so change that to your liking, if this is not what you want.
 const signatureMarker = "-- "
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: sv [genkey|sign|verify] [message file] [key file]")
+		fmt.Println("Usage: sv [genkey|sign|verify] [message file] [key file] [-l lineLength]")
 		return
 	}
 
-command := os.Args[1]
+	command := os.Args[1]
 
-switch command {
-case "genkey":
-	publicKey, privateKey, err := ed25519.GenerateKey(nil)
-	if err != nil {
-		log.Fatalf("Failed to generate key pair: %v", err)
-	}
+	switch command {
+	case "genkey":
+		publicKey, privateKey, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			log.Fatalf("Failed to generate key pair: %v", err)
+		}
 
-	publicKeyHex := hex.EncodeToString(publicKey)
-	privateKeyHex := hex.EncodeToString(privateKey)
+		publicKeyHex := hex.EncodeToString(publicKey)
+		privateKeyHex := hex.EncodeToString(privateKey)
 
-	publicKeyFile := "pubme"
-	privateKeyFile := "secme"
+		publicKeyFile := "pubme"
+		privateKeyFile := "secme"
 
-	err = ioutil.WriteFile(publicKeyFile, []byte(publicKeyHex), 0644)
-	if err != nil {
-		log.Fatalf("Failed to save public key: %v", err)
-	}
+		err = ioutil.WriteFile(publicKeyFile, []byte(publicKeyHex), 0644)
+		if err != nil {
+			log.Fatalf("Failed to save public key: %v", err)
+		}
 
-	err = ioutil.WriteFile(privateKeyFile, []byte(privateKeyHex), 0644)
-	if err != nil {
-		log.Fatalf("Failed to save private key: %v", err)
-	}
+		err = ioutil.WriteFile(privateKeyFile, []byte(privateKeyHex), 0644)
+		if err != nil {
+			log.Fatalf("Failed to save private key: %v", err)
+		}
 
-	fmt.Println("Key pair generated and saved in pubkey and privkey")
+		fmt.Println("Key pair generated and saved in pubkey and privkey")
 
-case "sign":
-	if len(os.Args) < 4 {
-		fmt.Println("Usage: sv sign [message file] [private key file]")
-		return
-	}
+	case "sign":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: sv sign [message file] [private key file] [-l lineLength]")
+			return
+		}
 
-	messageFile := os.Args[2]
-	keyFile := os.Args[3]
+		messageFile := os.Args[2]
+		keyFile := os.Args[3]
+		lineLength := 0 // Default to one long line
 
-	privateKeyBytes, err := ioutil.ReadFile(keyFile)
-	if err != nil {
-		log.Fatalf("Failed to read private key file: %v", err)
-	}
+		// Check if the user provided a line length as an integer using the -l option
+		for i := 4; i < len(os.Args); i++ {
+			if os.Args[i] == "-l" && i+1 < len(os.Args) {
+				providedLineLength, err := strconv.Atoi(os.Args[i+1])
+				if err != nil {
+					log.Fatalf("Invalid line length: %v", err)
+				}
+				lineLength = providedLineLength
+			}
+		}
 
-	privateKey, err := hex.DecodeString(string(privateKeyBytes))
-	if err != nil {
-		log.Fatalf("Failed to decode private key: %v", err)
-	}
+		privateKeyBytes, err := ioutil.ReadFile(keyFile)
+		if err != nil {
+			log.Fatalf("Failed to read private key file: %v", err)
+		}
 
-	messageBytes, err := ioutil.ReadFile(messageFile)
-	if err != nil {
-		log.Fatalf("Failed to read the message file: %v", err)
-	}
+		privateKey, err := hex.DecodeString(string(privateKeyBytes))
+		if err != nil {
+			log.Fatalf("Failed to decode private key: %v", err)
+		}
 
-	// Check for EOF
-	if len(messageBytes) > 0 && messageBytes[len(messageBytes)-1] != '\n' {
-		messageBytes = append(messageBytes, '\n')
-	}
+		messageBytes, err := ioutil.ReadFile(messageFile)
+		if err != nil {
+			log.Fatalf("Failed to read the message file: %v", err)
+		}
 
-	signature := signMessage(privateKey, messageBytes)
-	signatureHex := hex.EncodeToString(signature)
+		// Check for EOF
+		if len(messageBytes) > 0 && messageBytes[len(messageBytes)-1] != '\n' {
+			messageBytes = append(messageBytes, '\n')
+		}
 
-	// Append the signature with the marker
-	messageWithSignature := append(messageBytes, []byte(signatureMarker+"\n"+signatureHex)...)
+		signature := signMessage(privateKey, messageBytes)
+		signatureHex := hex.EncodeToString(signature)
 
-	err = ioutil.WriteFile(messageFile, messageWithSignature, 0644)
-	if err != nil {
-		log.Fatalf("Failed to save message with signature: %v", err)
-	}
+		// Split the signature into lines based on the specified line length or use a single line
+		var signatureLines []byte
+		if lineLength > 0 {
+			signatureLines = splitString(signatureHex, lineLength)
+		} else {
+			signatureLines = []byte(signatureHex)
+		}
 
-	fmt.Printf("Signature: %s\n", signatureHex)
+		// Append the signature lines with the marker
+		messageWithSignature := append(messageBytes, []byte(signatureMarker+"\n")...)
+		messageWithSignature = append(messageWithSignature, []byte(signatureLines)...)
 
-case "verify":
-	if len(os.Args) < 4 {
-		fmt.Println("Usage: sv verify [message file] [public key file]")
-		return
-	}
+		err = ioutil.WriteFile(messageFile, messageWithSignature, 0644)
+		if err != nil {
+			log.Fatalf("Failed to save message with signature: %v", err)
+		}
 
-	messageFile := os.Args[2]
-	keyFile := os.Args[3]
+		fmt.Printf("Signature: %s\n", signatureHex)
 
-	publicKeyBytes, err := ioutil.ReadFile(keyFile)
-	if err != nil {
-		log.Fatalf("Failed to read public key file: %v", err)
-	}
+	case "verify":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: sv verify [message file] [public key file]")
+			return
+		}
 
-	publicKey, err := hex.DecodeString(string(publicKeyBytes))
-	if err != nil {
-		log.Fatalf("Failed to decode public key: %v", err)
-	}
+		messageFile := os.Args[2]
+		keyFile := os.Args[3]
 
-	messageWithSignature, err := ioutil.ReadFile(messageFile)
-	if err != nil {
-		log.Fatalf("Failed to read the message with signature: %v", err)
-	}
+		publicKeyBytes, err := ioutil.ReadFile(keyFile)
+		if err != nil {
+			log.Fatalf("Failed to read public key file: %v", err)
+		}
 
-	// Check for the signature marker
-	parts := bytes.SplitN(messageWithSignature, []byte(signatureMarker), 2)
-	if len(parts) != 2 {
-		log.Fatalf("Invalid message format: missing signature marker")
-	}
+		publicKey, err := hex.DecodeString(string(publicKeyBytes))
+		if err != nil {
+			log.Fatalf("Failed to decode public key: %v", err)
+		}
 
-	messageBytes := parts[0]
-	signatureHex := bytes.TrimSpace(parts[1])
+		messageWithSignature, err := ioutil.ReadFile(messageFile)
+		if err != nil {
+			log.Fatalf("Failed to read the message with signature: %v", err)
+		}
 
-	valid := verifySignature(publicKey, signatureHex, messageBytes)
-	if valid {
-		fmt.Println("Signature is valid.")
-	} else {
-		fmt.Println("Signature is not valid.")
-	}
+		// Check for the signature marker
+		parts := bytes.SplitN(messageWithSignature, []byte(signatureMarker), 2)
+		if len(parts) != 2 {
+			log.Fatalf("Invalid message format: missing signature marker")
+		}
 
-default:
-	fmt.Println("Usage: sv [genkey|sign|verify] [message file] [key file]")
+		messageBytes := parts[0]
+		signatureHex := bytes.TrimSpace(parts[1])
+
+		// Remove line breaks and spaces from the signature
+		signatureHex = bytes.ReplaceAll(signatureHex, []byte("\n"), []byte(""))
+		signatureHex = bytes.ReplaceAll(signatureHex, []byte(" "), []byte(""))
+
+		valid := verifySignature(publicKey, signatureHex, messageBytes)
+		if valid {
+			fmt.Println("Signature is valid.")
+		} else {
+			fmt.Println("Signature is not valid.")
+		}
 	}
 }
 
 func signMessage(privateKey []byte, message []byte) []byte {
-return ed25519.Sign(privateKey, message)
+	return ed25519.Sign(privateKey, message)
 }
 
 func verifySignature(publicKey []byte, signatureHex []byte, message []byte) bool {
-signature, err := hex.DecodeString(string(signatureHex))
-if err != nil {
-log.Fatalf("Failed to decode signature: %v", err)
+	signature, err := hex.DecodeString(string(signatureHex))
+	if err != nil {
+		log.Fatalf("Failed to decode signature: %v", err)
+	}
+
+	return ed25519.Verify(publicKey, message, signature)
 }
 
-return ed25519.Verify(publicKey, message, signature)
+func splitString(s string, lineLength int) []byte {
+	length := len(s)
+	parts := []string{}
+
+	for i := 0; i < length; i += lineLength {
+		end := i + lineLength
+		if end > length {
+			end = length
+		}
+		parts = append(parts, s[i:end])
+	}
+
+	return []byte(strings.Join(parts, "\n"))
 }
+
